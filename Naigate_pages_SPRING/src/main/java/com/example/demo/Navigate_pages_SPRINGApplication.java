e12059ee7cfdaa79d30c2ecb943d0c96290912be
Lenovo <Lenovo@LAPTOP-86ONO92H>
2020-06-25 13:55:13 +0530

    Navigate between pages using Spring tool

---
+++ Naigate_pages_SPRING/src/main/java/com/example/demo/Navigate_pages_SPRINGApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Navigate_pages_SPRINGApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Navigate_pages_SPRINGApplication.class, args);
+	}
+
+}
