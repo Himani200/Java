fee65f73450ceaba9bc267fbc5fd06dd9e790f17
Lenovo <Lenovo@LAPTOP-86ONO92H>
2020-06-26 11:32:15 +0530

    Registration form

---
+++ Registrationform_SPRING/src/main/java/net/codejava/RegistrationformSpringApplication.java
@@ -0,0 +1,13 @@
+package net.codejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationformSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationformSpringApplication.class, args);
+	}
+
+}
